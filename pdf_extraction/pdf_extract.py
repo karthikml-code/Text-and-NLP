34826b9781bdc5e4e48599417d3de920008a4098
karthikml-code <85920192+karthikml-code@users.noreply.github.com>
2022-04-20 21:00:14 +0530

    Update pdf_extract.py

--- pdf_extraction/pdf_extract.py
+++ pdf_extraction/pdf_extract.py
@@ -3,3 +3,12 @@ file = pdfplumber.open('./any_file.pdf')
 full_text = []
 for page in range(0,len(file.pages)):
     full_text.append(file.pages[page].extract_text())
+    
+#here we are extracting the bold text from the pdf since the example contains headings in bold font
+headings = []
+with file as pdf: 
+    for page in range(3,len(file.pages)):
+        text = pdf.pages[page]
+        bold_text = text.filter(lambda obj: (obj["object_type"] == "char" and "Bold" in obj["fontname"]))
+        headings.append(bold_text.extract_text())
+        headings.append("\n")
